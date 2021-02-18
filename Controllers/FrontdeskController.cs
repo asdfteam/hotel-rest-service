3b629936029b7aa59f73522009b126a303f195e0
christianfoleide <christian.foleide@gmail.com>
2021-02-18 13:36:12 +0100

    nuget packages and dbcontext

---
+++ Controllers/FrontdeskController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using HotelLibrary;
+
+namespace hotelservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FrontdeskController : ControllerBase
+    {
+        private readonly ILogger<FrontdeskController> _logger;
+        private readonly HotelDbContext _hotelDbContext;
+
+        public FrontdeskController(ILogger<FrontdeskController> logger, HotelDbContext hotelDbContext)
+        {
+            _logger = logger;
+            _hotelDbContext = hotelDbContext;
+        }
+
+        [HttpGet("/reservations")]
+        public IEnumerable<Reservation> GetReservations()
+        {
+            List<Reservation> reservations = new List<Reservation>();
+            reservations.Add(new Reservation(100, 254, DateTime.Now, DateTime.Now.AddDays(2)));
+            reservations.Add(new Reservation(101, 199, DateTime.Now.AddDays(3), DateTime.Now.AddDays(5)));
+            reservations.Add(new Reservation(102, 423, DateTime.Now, DateTime.Now.AddDays(7)));
+            reservations.Add(new Reservation(103, 423, DateTime.Now, DateTime.Now.AddDays(10)));
+
+            return reservations;
+
+        }
+    }
+}
